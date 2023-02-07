8533871f96cd1c1fc673e490c9804d59742279b2
Elrinth <elrinth@gmail.com>
2023-02-07 12:09:58 +0100

    Initial commit of program

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace BinMerger
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.txtLog = new System.Windows.Forms.TextBox();
+      this.Log = new System.Windows.Forms.Label();
+      this.txtFolderBinCues = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.btnStart = new System.Windows.Forms.Button();
+      this.btnClearLog = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // txtLog
+      // 
+      this.txtLog.Location = new System.Drawing.Point(28, 95);
+      this.txtLog.Multiline = true;
+      this.txtLog.Name = "txtLog";
+      this.txtLog.Size = new System.Drawing.Size(611, 211);
+      this.txtLog.TabIndex = 0;
+      // 
+      // Log
+      // 
+      this.Log.AutoSize = true;
+      this.Log.Location = new System.Drawing.Point(28, 79);
+      this.Log.Name = "Log";
+      this.Log.Size = new System.Drawing.Size(25, 13);
+      this.Log.TabIndex = 1;
+      this.Log.Text = "Log";
+      // 
+      // txtFolderBinCues
+      // 
+      this.txtFolderBinCues.Location = new System.Drawing.Point(198, 42);
+      this.txtFolderBinCues.Name = "txtFolderBinCues";
+      this.txtFolderBinCues.Size = new System.Drawing.Size(211, 20);
+      this.txtFolderBinCues.TabIndex = 2;
+      this.txtFolderBinCues.TextChanged += new System.EventHandler(this.txtFolderBinCues_TextChanged);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(25, 45);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(158, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Root folder to find .bin/.cue files";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(415, 39);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "Browse...";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // btnStart
+      // 
+      this.btnStart.Enabled = false;
+      this.btnStart.Location = new System.Drawing.Point(564, 40);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(75, 23);
+      this.btnStart.TabIndex = 5;
+      this.btnStart.Text = "Start";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // btnClearLog
+      // 
+      this.btnClearLog.Location = new System.Drawing.Point(564, 69);
+      this.btnClearLog.Name = "btnClearLog";
+      this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+      this.btnClearLog.TabIndex = 6;
+      this.btnClearLog.Text = "Clear Log";
+      this.btnClearLog.UseVisualStyleBackColor = true;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(25, 9);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(382, 13);
+      this.label2.TabIndex = 7;
+      this.label2.Text = "Select a folder on your computer where you have .bin/.cue combination games.";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(25, 22);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(537, 13);
+      this.label3.TabIndex = 8;
+      this.label3.Text = "This will check all subdirectories for this and combine the .bins into single bin" +
+    " and update the .cue to refer to that.";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(672, 325);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.btnClearLog);
+      this.Controls.Add(this.btnStart);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.txtFolderBinCues);
+      this.Controls.Add(this.Log);
+      this.Controls.Add(this.txtLog);
+      this.Name = "Form1";
+      this.Text = "Bin Merger";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtLog;
+    private System.Windows.Forms.Label Log;
+    private System.Windows.Forms.TextBox txtFolderBinCues;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Button btnClearLog;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,274 @@
+﻿using BinMerger.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinMerger
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+      {
+        if (txtFolderBinCues.Text != "" && Directory.Exists(txtFolderBinCues.Text))
+        {
+          dlg.SelectedPath = txtFolderBinCues.Text;
+        }
+        else
+        {
+          dlg.SelectedPath = Directory.GetCurrentDirectory();
+        }
+        if (dlg.ShowDialog() != DialogResult.OK)
+        {
+          return;
+        }
+        txtFolderBinCues.Text = dlg.SelectedPath;
+        btnStart.Enabled = (txtFolderBinCues.Text != "");
+      }
+    }
+
+
+    private void btnStart_Click(object sender, EventArgs e)
+    {
+      DateTime startTime = DateTime.Now;
+
+
+      // find all bin/cue files:
+      string[] allCueFiles = null;
+      string[] allBinFiles = null;
+      try
+      {
+        allCueFiles = Directory.GetFiles(txtFolderBinCues.Text, "*.cue", SearchOption.AllDirectories);
+        allBinFiles = Directory.GetFiles(txtFolderBinCues.Text, "*.bin", SearchOption.AllDirectories);
+      }
+      catch (Exception ex)
+      {
+        txtLog.Invoke((MethodInvoker)delegate ()
+        {
+          txtLog.AppendText("Could not access folder: " + txtFolderBinCues.Text + " exception: " + ex.ToString() + "\r\n");
+        });
+        btnClearLog.Enabled = true;
+        return;
+      }
+      if (allCueFiles == null || allCueFiles.Length == 0)
+      {
+        txtLog.Invoke((MethodInvoker)delegate ()
+        {
+          txtLog.AppendText("No .cue files were found in the folder: " + txtFolderBinCues.Text + "! Are you sure you have .cue files there?\r\n");
+        });
+        btnClearLog.Enabled = true;
+        return;
+      }
+      if (allBinFiles == null || allBinFiles.Length == 0)
+      {
+        txtLog.Invoke((MethodInvoker)delegate ()
+        {
+          txtLog.AppendText("No .bin files were found in the folder: " + txtFolderBinCues.Text + "! Are you sure you have .bin files there?\r\n");
+          if (allCueFiles != null)
+          {
+            txtLog.AppendText("You do however have " + allCueFiles.Length + ".cue files in the folder! Are they already converted to .cdm?\r\n");
+          }
+        });
+        btnClearLog.Enabled = true;
+        return;
+      }
+      var approvedCueFiles = new List<string>();
+      long totalFileSize = 0;
+      var approvedBinFiles = new List<string>();
+
+      Dictionary<string, List<string>> binFilesPerCue = new Dictionary<string, List<string>>();
+      foreach (string cueFile in allCueFiles)
+      {
+        // read cue file to figure out which binfile we are looking for:
+        // try here?
+        string[] lines = null;
+        try
+        {
+          lines = File.ReadAllLines(cueFile);
+        }
+        catch (Exception ex)
+        {
+          txtLog.Invoke((MethodInvoker)delegate ()
+          {
+            txtLog.AppendText("Could not read: " + cueFile + "! " + ex.ToString() + "\r\n");
+          });
+        }
+        if (lines == null)
+          continue;
+        var binFileName = "";
+        var hadBinFile = false;
+        // read all lines to figure out which bin files we should actually remove afterwards if we selected remove...
+        foreach (var line in lines)
+        {
+          if (line.StartsWith("FILE \"") && line.EndsWith("\" BINARY"))
+          {
+            binFileName = line.Substring("FILE \"".Length, line.LastIndexOf("\" BINARY") - "FILE \"".Length);
+            if (binFileName != "" && binFileName.EndsWith(".bin"))
+            {
+              foreach (string binFile in allBinFiles)
+              {
+                if (binFile.Contains(binFileName))
+                {
+                  if (approvedBinFiles.Contains(binFile) == false)
+                  {
+                    if (!binFilesPerCue.ContainsKey(cueFile))
+                    {
+                      binFilesPerCue.Add(cueFile, new List<string>());
+                    }
+                    binFilesPerCue[cueFile].Add(binFile);
+                    approvedBinFiles.Add(binFile);
+                    // get file size:
+                    totalFileSize += new FileInfo(binFile).Length;
+                  }
+                  hadBinFile = true;
+                }
+              }
+            }
+          }
+        }
+
+        if (hadBinFile)
+        {
+          approvedCueFiles.Add(cueFile);
+        }
+      }
+
+      txtLog.Invoke((MethodInvoker)delegate ()
+      {
+        txtLog.AppendText("Found: " + approvedCueFiles.Count + " .cue files with .bin files. Conversion starting in 1 seconds...\r\n");
+      });
+      Task.Delay(1000).Wait();
+      txtLog.Invoke((MethodInvoker)delegate ()
+      {
+        txtLog.AppendText("1 second...\r\n");
+      });
+      Task.Delay(1000).Wait();
+
+      bool canceled = false;
+      var binFilesRemoved = 0;
+      var cueFilesRemoved = 0;
+      var countCueFilesProcessed = 0;
+      // CONVERT!
+      foreach (string filePath in approvedCueFiles)
+      {
+        if (!File.Exists(filePath))
+        {
+          txtLog.Invoke((MethodInvoker)delegate ()
+          {
+            txtLog.AppendText("Could not find cue any longer @" + filePath + "!\r\n");
+          });
+          continue;
+        }
+        var tempPath = Path.GetDirectoryName(filePath) + "\\temp";
+        var outpath = Path.GetDirectoryName(filePath);
+        var fName = Path.GetFileNameWithoutExtension(filePath);
+
+        // must create temp folder first!
+        if (!Directory.Exists(tempPath))
+        {
+          Directory.CreateDirectory(tempPath);
+        }
+
+        var proc = new Process
+        {
+          StartInfo = new ProcessStartInfo
+          {
+            FileName = "binmerge/binmerge.exe",
+            Arguments = "-o \"" + tempPath + "\" \"" + filePath + "\" \"" + fName + "\"",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+          }
+        };
+        proc.Start();
+        string outPut = proc.StandardOutput.ReadToEnd();
+        if (outPut.Contains("ERROR"))
+        {
+          txtLog.Invoke((MethodInvoker)delegate ()
+          {
+            txtLog.AppendText("Error occured when merging: " + outPut + "\r\n");
+          });
+          proc.WaitForExit();
+          proc.Close();
+          return;
+        }
+        txtLog.Invoke((MethodInvoker)delegate ()
+        {
+          txtLog.AppendText("Combined .bin/.cue cd image: " + (countCueFilesProcessed + 1) + " / " + approvedCueFiles.Count + " with file: " + filePath + ". " + outPut + "\r\n");
+        });
+        proc.WaitForExit();
+        var exitCode = proc.ExitCode;
+        proc.Close();
+
+        // delete old bin files
+        foreach (string binFile in binFilesPerCue[filePath])
+        {
+          if (File.Exists(binFile))
+          {
+            File.Delete(binFile);
+            binFilesRemoved++;
+          }
+        }
+        // delete old cue file
+        if (File.Exists(filePath))
+        {
+          File.Delete(filePath);
+          cueFilesRemoved++;
+        }
+        // move the temp files
+        File.Move(tempPath + "\\" + fName + ".bin", outpath + "\\" + fName + ".bin");
+        File.Move(tempPath + "\\" + fName + ".cue", outpath + "\\" + fName + ".cue");
+        // delete the temp folder
+        if (Directory.Exists(tempPath))
+        {
+          Directory.Delete(tempPath);
+        }
+
+
+        countCueFilesProcessed++;
+      }
+
+      TimeSpan diff = DateTime.Now - startTime;
+
+      txtLog.Invoke((MethodInvoker)delegate ()
+      {
+        txtLog.AppendText("Finished combining " + countCueFilesProcessed + " .bin/.cue files after " + Math.Round(diff.TotalSeconds, 2) + " seconds!\r\n");
+      });
+      if (binFilesRemoved != 0)
+      {
+        txtLog.Invoke((MethodInvoker)delegate ()
+        {
+          txtLog.AppendText("Deleted " + binFilesRemoved + " .bin files!\r\n");
+        });
+      }
+
+      btnClearLog.Enabled = true;
+    }
+
+    private void txtFolderBinCues_TextChanged(object sender, EventArgs e)
+    {
+      if (txtFolderBinCues.Text != "")
+      {
+        btnStart.Enabled = true;
+      } else
+      {
+        btnStart.Enabled = false;
+      }
+    }
+  }
+}
